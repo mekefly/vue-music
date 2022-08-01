208b2d361cc568e38a07c22c3a4ef941f34aeefc
meke <3197801970@qq.com>
2022-08-01 19:51:43 +0800

    fix: banner 轮播体的报错

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
+import { useEventListener, useIntervalFn } from "@vueuse/core";
 import { divRef } from "../utils/vue/refDom";
 import { computed, onMounted, Ref, ref, watchEffect } from "vue";
 import { BannerType, getBanner, GetBannerReturnType } from "../api/banner";
 
-const screenDom = divRef();
-const sliderDom = divRef();
+const screenDom: Ref<HTMLDivElement | null> = divRef();
+const sliderDom: Ref<HTMLDivElement | null> = divRef();
 
 const bannerData: Ref<GetBannerReturnType | null> = ref(null);
 watchEffect(async () => {
@@ -20,113 +21,138 @@ const banners: Ref<GetBannerReturnType["banners"]> = computed(() => {
 const len = computed(() => {
   return banners.value.length;
 });
-onMounted(() => {
-  let activeIndex = 0;
+let activeIndex = 0;
 
-  //拖动到对应的页面
+//拖动到对应的页面
 
-  //按下的状态
-  let isMousedown = false;
+//按下的状态
+let isMousedown = false;
 
-  //按下鼠标时记录一个除始状态
-  let mousedownPageX = 0;
-  screenDom.value.addEventListener("mousedown", function (e) {
-    isMousedown = true;
-    mousedownPageX = e.pageX;
-    screenDom.value.classList.add("mousedown");
-  });
+//按下鼠标时记录一个除始状态
+let mousedownPageX = 0;
+function mousedown(e: MouseEvent) {
+  isMousedown = true;
+  mousedownPageX = e.pageX;
+  screenDom.value?.classList.add("mousedown");
+}
 
-  document.addEventListener("mouseup", function (e) {
-    //鼠标弹起时标准更新
-    stdUpdate(e);
-    isMousedown = false;
-    screenDom.value.classList.remove("mousedown");
-  });
+useEventListener(document, "mouseup", mouseup);
+function mouseup(e: MouseEvent) {
+  //鼠标弹起时标准更新
+  stdUpdate(e);
+  isMousedown = false;
+  screenDom.value?.classList.remove("mousedown");
+}
 
-  //循环运行
-  setInterval(() => {
-    if (!isMousedown) {
-      activeIndex++;
-      if (activeIndex >= len.value) {
-        activeIndex = 0;
-      }
-      refreshRender();
+//循环运行
+setInterval(() => {
+  if (!isMousedown) {
+    activeIndex++;
+    if (activeIndex >= len.value) {
+      activeIndex = 0;
     }
-  }, 3000);
-
-  /**
-   * 这个更新会自动对齐
-   * @param {*} e
-   */
-  function stdUpdate(e: MouseEvent) {
-    if (isMousedown) {
-      const left = getLeft(e.pageX);
-      //当松开手指才会更新这个位置按住的时候只更新了页面的显示，没有更新它，
-      //如果您需要页面显示渲染更新的一个index可以使用
-      activeIndex = getActiveIndex(left);
-      // activeIndex 代表当前要显示哪一个然后*一个屏幕宽度就是一个整数屏幕的位置，就对齐了
-      render(-activeIndex * getScreenWidth(), activeIndex);
-    }
-  }
-  let screenRect = screenDom.value.getBoundingClientRect();
-  //windows resize
-  window.addEventListener("resize", function () {
-    screenRect = screenDom.value.getBoundingClientRect();
-
+    resize();
     refreshRender();
-  });
+  }
+}, 3000);
 
-  //移动
-  document.addEventListener("mousemove", update);
-  function update(e: MouseEvent) {
-    if (isMousedown) {
-      const left = getLeft(e.pageX);
-      const index = getActiveIndex(left);
-      render(left, index);
-    }
+/**
+ * 这个更新会自动对齐
+ * @param {*} e
+ */
+function stdUpdate(e: MouseEvent) {
+  if (isMousedown) {
+    const left = getLeft(e.pageX);
+    //当松开手指才会更新这个位置按住的时候只更新了页面的显示，没有更新它，
+    //如果您需要页面显示渲染更新的一个index可以使用
+    activeIndex = getActiveIndex(left);
+    // activeIndex 代表当前要显示哪一个然后*一个屏幕宽度就是一个整数屏幕的位置，就对齐了
+    render(-activeIndex * getScreenWidth(), activeIndex);
   }
+}
+let screenRect: DOMRect | null | undefined = null;
 
-  function refreshRender() {
-    const left = -activeIndex * getScreenWidth();
-    render(left, activeIndex);
+onMounted(resize);
+useEventListener(screenDom.value, "DOMContentLoaded", resize);
+useEventListener(window, "resize", resize);
+function resize() {
+  if (!screenDom.value) {
+    return;
   }
-  function render(left: number, index: number) {
-    sliderDom.value.style.transform = `translate(${left}px)`;
-  }
+  screenRect = screenDom.value.getBoundingClientRect();
 
-  function getLeft(pageX: number) {
-    const left = -activeIndex * getScreenWidth() + pageX - mousedownPageX;
-    return left;
-  }
-  function getActiveIndex(left: number) {
-    const width = getScreenWidth();
-    //left是负数，最终要得到正数index
-    const i = Math.floor((-left + width / 2) / width);
-    if (i < 0) {
-      return 0;
-    }
-    if (i >= len.value - 1) {
-      return len.value - 1;
-    }
-    return i;
-  }
+  refreshRender();
+}
 
-  function getScreenWidth() {
-    return getScreenRect().width;
+//移动
+useEventListener(document, "mousemove", update);
+function update(e: MouseEvent) {
+  if (isMousedown) {
+    const left = getLeft(e.pageX);
+    const index = getActiveIndex(left);
+    render(left, index);
   }
-  function getScreenRect() {
-    return screenRect;
+}
+
+function refreshRender() {
+  const left = -activeIndex * getScreenWidth();
+  render(left, activeIndex);
+}
+function render(left: number, index: number) {
+  if (!sliderDom.value) {
+    return;
   }
-});
+  sliderDom.value.style.transform = `translate(${left}px)`;
+}
+
+function getLeft(pageX: number) {
+  const left = -activeIndex * getScreenWidth() + pageX - mousedownPageX;
+  return left;
+}
+function getActiveIndex(left: number) {
+  const width = getScreenWidth();
+  //left是负数，最终要得到正数index
+  const i = Math.floor((-left + width / 2) / width);
+  if (i < 0) {
+    return 0;
+  }
+  if (i >= len.value - 1) {
+    return len.value - 1;
+  }
+  return i;
+}
+
+function getScreenWidth() {
+  const rect = getScreenRect();
+
+  if (!rect) {
+    return 0;
+  }
+  return rect.width;
+}
+function getScreenRect() {
+  return screenRect;
+}
 </script>
 
 <template>
-  <div class="screen" ref="screenDom" draggable="false">
+  <div class="screen" draggable="false" ref="screenDom" @mousedown="mousedown">
     <div class="body" draggable="false">
       <div class="slider" ref="sliderDom" draggable="false">
         <div class="item" v-for="banner in banners" draggable="false">
           <a :href="banner.url" draggable="false">
-            <img :src="banner.pic" :alt="banner.typeTitle" draggable="false" />
+            <img
+              :src="banner.pic"
+              :alt="banner.typeTitle"
+              draggable="false"
+              @click="
+                (e:MouseEvent) => {
+                  if (Math.abs(e.pageX - mousedownPageX)>10) {
+                    e.preventDefault();
+                  }
+                }
+              "
+            />
           </a>
         </div>
       </div>
